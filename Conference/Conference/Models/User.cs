eab083756ed046d3904dc043b068f66d0376f17a
pedro154 <pedro.ferreira154@gmail.com>
2016-02-29 09:57:45 -0300

    Colocando (User Class) dentro da pasta model

--- Conference/Conference/Models/User.cs
+++ Conference/Conference/Models/User.cs
@@ -11,6 +11,6 @@ namespace Conference
         public string Username { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
-        public int Kind { get; set; }
+        public int Kind { get; set; } 
     }
 }
