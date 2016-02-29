0a39c1905cae47e314193e01a76ade0a85ad4fdc
Rodrigo Augusto de Carvalho <racarvalho1987@gmail.com>
2016-02-29 09:53:01 -0300

    Criação da Classe Presenter

---
+++ Conference/Conference/Models/Presenter.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Conference.Models {
+    public class Presenter {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Title { get; set; }
+        public string Biography { get; set; }
+        public string User_Owner { get; set; }       
+    }
+}
